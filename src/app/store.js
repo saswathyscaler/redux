4400735812208447f5854641afc93fa07eceafb3
saswat <saswatr.mohanty@hyscaler.com>
2023-10-20 16:08:13 +0530

    .................

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import dashboardSlice from "dashboard/features/dashboardSlice";
+import userSlice from "dashboard/features/userSlice";
 
 const store = configureStore({
   reducer: {
     dashboard: dashboardSlice,
+    users : userSlice
   },
 });
 export default store;
